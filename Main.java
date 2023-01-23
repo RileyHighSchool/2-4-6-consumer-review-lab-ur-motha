31b14fd8677725f11816aa78240ee96514f534e3
Catrell24 <catrell91246@students.sbcsc.k12.in.us>
2023-01-23 08:51:43 -0500

    main

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Review.sentimentVal("bad"));
+        System.out.println(Review.sentimentVal("good"));
     }
 }
